4925bd73552601c3e9afbf22a1c5ef97552d1d29
jfernandesdev <jfernandes.dev@gmail.com>
2021-09-01 23:02:47 -0300

    layout and routing of the episodes internal pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,8 @@
+import '../styles/globals.scss'
+
 import { Header } from '../components/Header'
 import { Player } from '../components/Player'
 
-import '../styles/globals.scss'
 import styles from '../styles/app.module.scss'
 
 function MyApp({ Component, pageProps }) {
