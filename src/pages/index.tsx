372e84fad30e8934f898ce5c25e27e6a14f565ae
Jeferson Fernandes <89709787+jfernandesdev@users.noreply.github.com>
2021-09-03 00:02:47 -0300

    Update import Link index pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { GetStaticProps } from 'next'
 import Image from 'next/image'
-import Link from 'next/Link'
+import Link from 'next/link'
 import Head from 'next/head'
 
 import { format, parseISO } from 'date-fns'
