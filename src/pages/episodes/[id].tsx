48f843dfdb374f72b5c7cfab42f0d7436b64201f
Jeferson Fernandes <89709787+jfernandesdev@users.noreply.github.com>
2021-09-03 00:00:10 -0300

    Update import Link pages episodes

--- src/pages/episodes/[id].tsx
+++ src/pages/episodes/[id].tsx
@@ -2,7 +2,7 @@ import { GetStaticPaths, GetStaticProps } from 'next';
 
 import Image from 'next/image';
 import Head from 'next/head';
-import Link from 'next/Link';
+import Link from 'next/link';
 
 import { api } from '../../services/api';
 import { usePlayer } from '../../contexts/PlayerContexts'
